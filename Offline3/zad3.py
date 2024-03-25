8a12f101152b9b00323d4ce072e043805e439257
Mistrz Egiptu <mistrzegiptu@gmail.com>
2024-03-25 04:18:10 +0100

    bruteforce

--- Offline3/zad3.py
+++ Offline3/zad3.py
@@ -1,3 +1,5 @@
+import math
+
 from zad3testy import runtests
 
 def heapify(T, n, i, coord):
@@ -5,9 +7,9 @@ def heapify(T, n, i, coord):
   r = 2 * i + 2
   maxInd = i
 
-  if l < n and T[l][coord] > T[maxInd][coord]:
+  if l < n and T[l][coord] < T[maxInd][coord]:
     maxInd = l
-  if r < n and T[r][coord] > T[maxInd][coord]:
+  if r < n and T[r][coord] < T[maxInd][coord]:
     maxInd = r
   if maxInd != i:
     T[maxInd], T[i] = T[i], T[maxInd]
@@ -23,17 +25,36 @@ def heapSort(T, coord):
   for i in range(n-1, 0, -1):
     T[i], T[0] = T[0], T[i]
     heapify(T, i, 0, coord)
-def dominance(P):
+'''def dominance(P):
   heapSort(P, 0)
-  #print(P)
-  heapSort(P, 1)
-  #print(P)
-  dominant = P[-1]
+  dominant = P[0]
+  for i in range(len(P)):
+      if P[i][1] > dominant[1]:
+        dominant = P[i]
   count = 0
   for i in range(len(P)):
       if P[i][0] < dominant[0] and P[i][1] < dominant[1]:
           count += 1
-  return count
+  return count'''
+'''def dominance(P):
+  maxDomina = 0
+  heapSort(P,0)
+  for i in range(len(P)//2):
+    currentDomina = 0
+    for j in range(i+1,len(P)):
+      if P[i][0] > P[j][0] and P[i][1] > P[j][1]:
+        currentDomina += 1
+    maxDomina = max(maxDomina, currentDomina)
+  return maxDomina'''
+def dominance(P):
+  maxDomina = 0
+  for point1 in P:
+    currentDomina = 0
+    for point2 in P:
+      if point1[1] > point2[1] and point1[0] > point2[0]:
+        currentDomina += 1
+    maxDomina = max(maxDomina, currentDomina)
+  return maxDomina
 
 # zmien all_tests na True zeby uruchomic wszystkie testy
 runtests( dominance, all_tests = True )
