206f94a10674ff0dd05441ca1b4d37fbba8b12f6
Mistrz Egiptu <mistrzegiptu@gmail.com>
2024-06-10 03:32:32 +0200

    Finished Offline 7

--- Offline7/zad7.py
+++ Offline7/zad7.py
@@ -1,26 +1,53 @@
+''' Franciszek Jawor
+W tym algorytmie wykorzystuje 2 tablice, Up przechowuje maksymalną liczbę komnat, która można odwiedzić przychodząc z lewej lub z góry,
+a Down- przychodząc z lewej lub z dołu. Algorytm przechodzi przez wszystkie komnaty i aktualizuje obie tablice, biorąc maksymalną
+ilość komnat możliwą do odwiedzenia na danym kroku.
+
+Złożoność pamięciową i czasową oceniam na O(n^2)
+'''
 from zad7testy import runtests
 
 def maze(L):
     n = len(L)
-    if L[0][0] == '#' or L[n - 1][n - 1] == '#':
-        return -1
 
-    dp = [[0] * n for _ in range(n)]
-    dp[0][0] = 1
+    Up = [[0] * n for _ in range(n)]
+    Down = [[0] * n for _ in range(n)]
 
-    for i in range(n):
-        for j in range(n):
+    #looking for wrong chambers
+    for j in range(n):
+        for i in range(n):
             if L[i][j] == '#':
-                continue
-            if i > 0 and dp[i - 1][j] > 0:
-                dp[i][j] = max(dp[i][j], dp[i - 1][j] + 1)
-            if j > 0 and dp[i][j - 1] > 0:
-                dp[i][j] = max(dp[i][j], dp[i][j - 1] + 1)
-            if i < n - 1 and dp[i + 1][j] > 0:
-                dp[i][j] = max(dp[i][j], dp[i + 1][j] + 1)
+                Up[i][j] = -float('inf')
+                Down[i][j] = -float('inf')
 
-    result = dp[n - 1][n - 1]
-    return result - 1 if result > 0 else -1
+    #starting values
+    for i in range(1, n):
+        Up[i][0] += Up[i-1][0] + 1
+        Up[0][i] += Up[0][i-1] + 1
+        Down[i][0] = -float('inf')
+        Down[0][i] += Down[0][i-1] + 1
+
+    for i in range(1, n):
+
+        if(L[0][i] != '#'):
+            Up[0][i] = max(Up[0][i-1], Down[0][i-1]) + 1
+
+        if (L[n-1][i] != '#'):
+            Down[n-1][i] = max(Up[n-1][i - 1], Down[n-1][i - 1]) + 1
+
+        for j in range(1, n):
+            if(L[j][i] == '#'):
+                continue
+            Up[j][i] = max(Up[j][i-1], Down[j][i-1], Up[j-1][i]) + 1
+
+        for j in range(n-2, -1, -1):
+            if(L[j][i] == '#'):
+                continue
+            Down[j][i] = max(Up[j][i-1], Down[j][i-1], Down[j+1][i]) + 1
+
+    if Up[n-1][n-1] == -float('inf') and Down[n-1][n-1] == -float('inf'):
+        return -1
+    return Up[n-1][n-1]
 
 # zmien all_tests na True zeby uruchomic wszystkie testy
-runtests( maze, all_tests = False )
+runtests( maze, all_tests = True )
